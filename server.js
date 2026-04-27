6b2714e57907bcb0fcd29b63deff4e87169d6140
unknown <enzo.giordani57@gmail.com>
2026-04-27 12:51:37 +0200

    feat: setup architecture modulaire, serveur et tests services

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-// On importera le routeur principal plus tard
 // const router = require('./routes/index.js');
 // const connectDB = require('./config/mongoDB.js');
 
@@ -10,8 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares globaux
-app.use(cors()); // Autoriser ton frontend React à communiquer avec le back
-app.use(express.json({ limit: "1mb" })); // Pour lire les body en JSON
+app.use(cors());
+app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 
 // Initialisation de la BDD
