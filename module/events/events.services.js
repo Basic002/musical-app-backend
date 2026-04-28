fc7c77d0124191b70e05f023ba0549ac66da96b1
unknown <enzo.giordani57@gmail.com>
2026-04-28 13:28:02 +0200

    feat: ajout de la logique métier dans les services avec de fausses données

--- module/events/events.services.js
+++ module/events/events.services.js
@@ -1,4 +1,15 @@
-export const testEventsService = () => {
-  console.log("Le service Events a été appelé !");
-  return { success: true, message: "Service Événements opérationnel" };
+export const getAllEvents = () => {
+  console.log("Service Events : Récupération de la liste des événements...");
+  // Plus tard : return await Event.find();
+  return [
+    { id: 1, artist: "Arctic Monkeys", venue: "Accor Arena, Paris", date: "12 Mai 2026" },
+    { id: 2, artist: "Justice", venue: "Le Zénith, Paris", date: "Ce soir" },
+    { id: 3, artist: "L'Impératrice", venue: "La Cigale, Paris", date: "18 Juin 2026" }
+  ];
+};
+
+export const getEventById = (eventId) => {
+  console.log(`Service Events : Récupération de l'événement avec l'ID ${eventId}`);
+  // Plus tard : return await Event.findById(eventId);
+  return { id: eventId, artist: "Artiste Test", venue: "Salle Test" };
 };
