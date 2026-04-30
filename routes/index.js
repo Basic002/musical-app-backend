58a4b82b9db6dc7849739c94520888598f36b554
unknown <enzo.giordani57@gmail.com>
2026-04-30 12:43:01 +0200

    feat(core): branchement des routeurs sur le serveur et test Postman validé

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import authRouter from '../module/auth/auth.routes.js';
+import eventsRouter from '../module/events/events.routes.js';
+import usersRouter from '../module/users/users.routes.js';
 
 const router = express.Router();
 
-router.use('/auth', authRouter);
+// Branchement des modules
+router.use('/auth', authRouter);   // Toutes les routes d'auth commenceront par /api/auth
+router.use('/events', eventsRouter); // Toutes les routes d'événements commenceront par /api/events
+router.use('/users', usersRouter);   // Toutes les routes utilisateurs commenceront par /api/users
 
+// Gestion de l'erreur 404
 router.use((req, res) => {
-  res.status(404).json({ error: "Route introuvable" });
+  res.status(404).json({ success: false, message: "Route API introuvable" });
 });
 
 export default router;
--- server.js
+++ server.js
@@ -2,24 +2,22 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import connectDB from './config/mongoDB.js'; 
-
-// On commentera ces lignes jusqu'à ce que les routes soient prêtes (Jeudi)
-// import router from './routes/index.js';
+import router from './routes/index.js'; // Le routeur principal est importé ici
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Connexion à la base de données
 // connectDB();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// On commente aussi l'utilisation du routeur
-// app.use('/api', router);
+// Branchement de toutes tes routes sous le préfixe /api
+app.use('/api', router);
 
-// Petite route de test basique en attendant
-app.get('/', (req, res) => res.send('API Discover en ligne (Version ESM) !'));
-
+// Démarrage du serveur
 app.listen(PORT, () => {
   console.log(`🚀 Serveur démarré sur http://localhost:${PORT}`);
 });
