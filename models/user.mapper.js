99f96d668ed844157ff547ec72837aa46554b015
unknown <enzo.giordani57@gmail.com>
2026-05-11 12:15:14 +0200

    feat(models): création du mapper utilisateur

---
+++ models/user.mapper.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+import CoreMapper from './core.mapper.js';
+import userSchema from '../schemas/user.schema.js';
+
+class UserMapper extends CoreMapper {
+    constructor() {
+        super(mongoose); 
+        this.model = this.mongoose.model('User', userSchema);
+    }
+
+    async findAll() {
+        return await this.model.find();
+    }
+
+    async findById(id) {
+        return await this.model.findById(id);
+    }
+
+    async findByEmail(email) {
+        return await this.model.findOne({ email });
+    }
+
+    async create(data) {
+        return await this.model.create(data);
+    }
+
+    async update(id, data) {
+        return await this.model.findByIdAndUpdate(id, data, { new: true });
+    }
+
+    async delete(id) {
+        return await this.model.findByIdAndDelete(id);
+    }
+}
+
+export default new UserMapper();
